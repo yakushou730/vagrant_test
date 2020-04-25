7da13a7b56ac2a44fec3af1958d432eeba3de57d
Shou Tseng <yakushou730@gmail.com>
2020-04-26 00:20:35 +0800

    Add basic process

--- script.sh
+++ script.sh
@@ -1,21 +1,29 @@
 #! /bin/zsh
 
-cd lb01
-vagrant destroy -f
-vagrant up
-cd ..
+if [ "$1" = "all" ] || [ "$1" = "lb01" ]; then
+  cd lb01
+  vagrant destroy -f
+  vagrant up
+  cd ..
+fi
 
-cd app01
-vagrant destroy -f
-vagrant up
-cd ..
+if [ "$1" = "all" ] || [ "$1" = "app01" ]; then
+  cd app01
+  vagrant destroy -f
+  vagrant up
+  cd ..
+fi
 
-cd app02
-vagrant destroy -f
-vagrant up
+if [ "$1" = "all" ] || [ "$1" = "app02" ]; then
+  cd app02
+  vagrant destroy -f
+  vagrant up
 cd ..
+fi
 
-cd db01
-vagrant destroy -f
-vagrant up
-cd ..
+if [ "$1" = "all" ] || [ "$1" = "db01" ]; then
+  cd db01
+  vagrant destroy -f
+  vagrant up
+  cd ..
+fi
